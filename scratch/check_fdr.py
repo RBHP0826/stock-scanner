91538ef302919b2e15f03332dfd34b8955ba7a18
RBHP0826 <a01055686356@gmail.com>
2026-06-12 10:42:44 +0900

    Delete scratch directory

--- scratch/check_fdr.py
+++
@@ -1,7 +0,0 @@
-import FinanceDataReader as fdr
-try:
-    df_desc = fdr.StockListing('KRX-DESC')
-    print("KRX-DESC Columns:", df_desc.columns.tolist())
-    print(df_desc[['Code', 'Name', 'Sector', 'Industry']].head(5))
-except Exception as e:
-    print("KRX-DESC Error:", e)
